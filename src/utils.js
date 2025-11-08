0da83679785cb95906e47657617189136cc44695
San-quan <6069996@gmail.com>
2025-11-09 05:18:47 +0800

    style: fix ESLint function spacing

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@
  * Utility functions for museStarlinkpay71
  */
 
-function isValidIPv4(ip) {
+function isValidIPv4 (ip) {
   const ipv4Regex = /^(\d{1,3}\.){3}\d{1,3}$/
   if (typeof ip !== 'string' || !ipv4Regex.test(ip)) return false
 
@@ -15,7 +15,7 @@ function isValidIPv4(ip) {
   })
 }
 
-function greet(name = 'world') {
+function greet (name = 'world') {
   return `Hello, ${name}! This repo integrates with Starlink.`
 }
 
